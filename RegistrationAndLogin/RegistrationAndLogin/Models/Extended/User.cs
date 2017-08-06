cab08b9b9ceddde89f17a207821945890e0d2028
Stathis Votsis <stathisvotsis@gmail.com>
2017-08-06 19:24:29 +0300

    asd

---
+++ RegistrationAndLogin/RegistrationAndLogin/Models/Extended/User.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace RegistrationAndLogin.Models
+{
+    [MetadataType(typeof(UserMetadata))]
+    public partial class User
+    {
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class UserMetadata
+    {
+        [Display(Name ="First Name")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "First Name Required")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Last Name Required")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Email Id")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email Required")]
+        public string EmailId { get; set; }
+
+        [Display(Name = "Date of birth")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "0:MM/dd/yyyy")]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        [MinLength(6,ErrorMessage = "Minimum 6 characters")]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirm Required")]
+        [DataType(DataType.Password)]
+        [Compare("Password",ErrorMessage = "Confirm password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
